975037a567c75eed7f074f70296cab865951b701
MBoota <m43577535@gmail.com>
2023-07-19 14:34:37 +0500

    1. convert to localhost 2. Added Frontend UI 3. Added Dashboard UI

--- OSCms/routes/web.php
+++ OSCms/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FrontController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+//_____________ frontend __________________
+#region frontent Route
+Route::get('/',[FrontController::class, "Index"]);
+#endregion
+
+//_____________ backend __________________
+#region backend Route
+Route::get('/dasbhoard/Index',[DashboardController::class, "Index"]);
+
+#endregion
+
